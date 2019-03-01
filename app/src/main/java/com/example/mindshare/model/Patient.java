940c0fd1f93bc4af28f12d6e3f6042d14a8cdb11
MaximCoppieters <maximcoppieters@gmail.com>
2019-03-01 21:53:10 +0100

    Added Caregiver, Patient and mock repos

---
+++ app/src/main/java/com/example/mindshare/model/Patient.java
@@ -0,0 +1,38 @@
+package com.example.mindshare.model;
+
+public class Patient {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+
+    public Patient(String firstName, String lastName, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
