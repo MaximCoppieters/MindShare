7a41508d0e67af8baee8238f7afc271caad076f1
fabioPuissant <fabio.puissant@hotmail.com>
2019-03-01 23:42:23 +0100

    main activity linked to optionsMenuCareTaker activity + commend colors

--- app/src/main/java/com/example/mindshare/MainActivity.java
+++ app/src/main/java/com/example/mindshare/MainActivity.java
@@ -1,31 +1,46 @@
 package com.example.mindshare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.MenuView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.example.mindshare.model.Caregiver;
+import com.example.mindshare.repo.CaregiverRepository;
+import com.example.mindshare.repo.PatientRepository;
 
 public class MainActivity extends AppCompatActivity {
 
+private CaregiverRepository caregiverRepository;
+private PatientRepository patientRepository;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        caregiverRepository = new CaregiverRepository();
+        patientRepository = new PatientRepository();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+       ImageView careTaker = findViewById(R.id.supportiveSide);
+        careTaker.setOnClickListener((view)-> {
+            caregiverRepository = caregiverRepository.getInstance();
+            startActivity(new Intent(MainActivity.this, OptionsMenuCareTaker.class));
+
         });
+
+        findViewById(R.id.patientSide).setOnClickListener((view)->{
+            patientRepository = patientRepository.getInstance();
+        });
+
     }
 
     @Override
